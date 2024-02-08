a9f34462c6deb16019bc10b689de9065afa7100e
Peter Jarvis <peterjza@gmail.com>
2024-02-08 18:42:33 +0200

    feat: linked list pop push

---
+++ linkedLists/index.js
@@ -0,0 +1,59 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class LinkedList {
+	constructor(value) {
+		const newNode = new Node(value);
+		this.head = newNode;
+		this.tail = this.head;
+		this.length = 1;
+	}
+
+	push(value) {
+		const newNode = new Node(value);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+
+		this.length++;
+	}
+
+	pop() {
+		if (!this.head) return undefined;
+		let pre = this.head;
+		let temp = this.head;
+
+		while (temp.next) {
+			pre = temp;
+			temp = temp.next;
+		}
+
+		this.tail = pre;
+		this.tail.next = null; // important this removes the link to the last node!
+		this.length--;
+
+		if (this.length === 0) {
+			this.head = null;
+			this.tail = null;
+		}
+
+		return temp;
+	}
+}
+
+const list = new LinkedList(1);
+list.push(2);
+list.push(3);
+list.pop();
+list.pop();
+list.pop();
+list.push(1);
+console.log(list);
